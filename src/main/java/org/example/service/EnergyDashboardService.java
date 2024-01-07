7872cbfb58d5b7a82b7c049dc13cf7ea2680d4d1
Maksym Rohachov <romax982@gmail.com>
2024-01-07 14:11:35 +0100

    Update EnergyDashboardService

--- src/main/java/org/example/service/EnergyDashboardService.java
+++ src/main/java/org/example/service/EnergyDashboardService.java
@@ -2,12 +2,25 @@ package org.example.service;
 
 import org.example.entity.EnergyDashboard;
 import java.util.List;
+import org.example.repository.EnergyDashboardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *  A service class to manage business logic and interact with the repository.
  *  This class can be used to retrieve data for the dashboard.
  */
+@Service
 public class EnergyDashboardService {
+
+    private final EnergyDashboardRepository energyDashboardRepository;
+
+    @Autowired
+    public EnergyDashboardService(EnergyDashboardRepository energyDashboardRepository) {
+        this.energyDashboardRepository = energyDashboardRepository;
+    }
+
     public List<EnergyDashboard> getAllEnergyData() {
+        return energyDashboardRepository.findAll();
     }
 }
