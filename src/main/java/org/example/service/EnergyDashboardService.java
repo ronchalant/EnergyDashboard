cc6c420ae1444122d62781cd9ac8ce122686752f
Maksym Rohachov <romax982@gmail.com>
2024-01-07 14:02:59 +0100

    Update POM

--- src/main/java/org/example/service/EnergyDashboardService.java
+++ src/main/java/org/example/service/EnergyDashboardService.java
@@ -1,8 +1,13 @@
 package org.example.service;
 
+import org.example.entity.EnergyDashboard;
+import java.util.List;
+
 /**
  *  A service class to manage business logic and interact with the repository.
  *  This class can be used to retrieve data for the dashboard.
  */
 public class EnergyDashboardService {
+    public List<EnergyDashboard> getAllEnergyData() {
+    }
 }
